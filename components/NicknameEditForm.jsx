9443a621f9704818d114862e9f97edc7c005eb1f
bhg1994 <hiswords1994@naver.com>
2019-10-16 05:10:40 +0900

    profile

--- components/NicknameEditForm.jsx
+++ components/NicknameEditForm.jsx
@@ -5,22 +5,39 @@ import {
   InputNickname,
   UpdateBtn
 } from "../components/css/NicknameEditForm";
+import { useDispatch, useSelector } from "react-redux";
+import { LOAD_USER_REQUEST } from "../reducers/user";
+
 
 const NicknameEditForm = () => {
+  const dispatch = useDispatch();
+  const {me} = useSelector(state => state.user);
+
+  console.log("nicknamecard "+me);
+
+
+
+  const onClicked = () =>{
+    dispatch({
+      type: LOAD_USER_REQUEST,
+    });
+
+  }
   return (
     <NickEditForm>
       <div>
         <Card.Meta
           style={{ marginTop: "2px" }}
           avatar={<Avatar>S </Avatar>}
-          title="Seong_Jin"
+          title={me.name}
         />
-        <LogoutBtn>로그아웃</LogoutBtn>
+        <LogoutBtn onClick={onClicked}>로그아웃</LogoutBtn>
       </div>
       <InputNickname addonBefore="닉네임" />
       <UpdateBtn type="primary">수정</UpdateBtn>
     </NickEditForm>
   );
 };
+
 
 export default NicknameEditForm;
