63bade706b6924d939a7c2f82cb6baa7798661ad
JayKim3 <lueky4300@naver.com>
2019-10-28 19:33:47 +0900

    Create LectureRoom

--- reducers/room.js
+++ reducers/room.js
@@ -1,9 +1,9 @@
 export const initialState = {
-    reservationErrorReason: '',
-    roomListErrorReason: '',
+    reservationErrorReason: "",
+    roomListErrorReason: "",
+    createRoomReason: "",
     isLoading: false,
     totalRoomList: [],
-    
 };
 
 export const RESERVATION_REQUEST = 'RESERVATION_REQUEST';
@@ -14,20 +14,26 @@ export const ROOMLIST_REQUEST = 'ROOMLIST_REQUEST';
 export const ROOMLIST_SUCCESS = 'ROOMLIST_SUCCESS';
 export const ROOMLIST_FAILURE = 'ROOMLIST_FAILURE';
 
+export const CREATEROOM_REQUEST = 'CREATEROOM_REQUEST';
+
+export const CREATEROOM_SUCCESS = 'CREATEROOM_SUCCESS';
+
+export const CREATEROOM_FAILURE = 'CREATEROOM_FAILURE';
+
 export default (state = initialState, action) => {
     switch (action.type) {
         case RESERVATION_REQUEST: {
             return {
                 ...state,
                 isLoading: true,
-                reservationErrorReason: '',
+                reservationErrorReason: ""
             };
         }
         case RESERVATION_SUCCESS: {
             return {
                 ...state,
                 isLoading: false,
-                
+
             };
         }
         case RESERVATION_FAILURE: {
@@ -41,14 +47,14 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 isLoading: true,
-                roomListErrorReason: '',
+                roomListErrorReason: ""
             };
         }
         case ROOMLIST_SUCCESS: {
             return {
                 ...state,
                 isLoading: false,
-                
+
             };
         }
         case ROOMLIST_FAILURE: {
@@ -58,8 +64,28 @@ export default (state = initialState, action) => {
                 roomListErrorReason: action.error,
             };
         }
+        case CREATEROOM_REQUEST: {
+            return {
+                ...state,
+                isLoading: true,
+                createRoomReason: ""
+            }
+        }
+        case CREATEROOM_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+            }
+        }
+        case CREATEROOM_FAILURE: {
+            return {
+                ...state,
+                isLoading: false,
+                createRoomReason: action.error
+            }
+        }
         default: {
-            return{
+            return {
                 ...state,
             };
         }
