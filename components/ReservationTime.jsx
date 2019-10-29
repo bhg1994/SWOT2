3881c71f1b20cc2f4246be4215dc563f5e47dd10
unknown <hiswords1994@naver.com>
2019-10-29 19:53:50 +0900

    reservation form

--- components/ReservationTime.jsx
+++ components/ReservationTime.jsx
@@ -2,6 +2,35 @@ import { Table } from "antd";
 import { ReservationTimeWrapper, Hoursofuse, Availabletime, SelecttimeWrapper, SelecttimeBtn } from '../components/css/ReservationTime';
 const { Column } = Table;
 
+const data =[
+  {
+    "roomId": 1,
+    "userId": 1,
+    "reson" : "study",
+    "phone" : "010",
+    "startTime" : "12",
+    "endTime" : "15",
+    "reservationDate" : 2019-12-11,
+    "state":'C',
+    "createdDate": "2019-10-05T08:08:49.000+0000", 
+    "updatedDate": "2019-10-05T08:08:49.000+0000"
+  },
+  {
+    "roomId": 1,
+    "userId": 1,
+    "reson" : "study",
+    "phone" : "010",
+    "startTime" : "16",
+    "endTime" : "17",
+    "reservationDate" : 2019-12-11,
+    "state":'C',
+    "createdDate": "2019-10-05T08:08:49.000+0000", 
+    "updatedDate": "2019-10-05T08:08:49.000+0000"
+  }
+
+] 
+
+
 const ReservationTime = ({ value }) => {
   const times = [
     "00:00",
@@ -29,6 +58,67 @@ const ReservationTime = ({ value }) => {
     "22:00",
     "23:00",
   ];
+  
+  var justClickedId ="";
+  var startId = "";
+  var beforeId = "";
+
+
+  const onClick = (e) => {
+    console.log(e.target.id);
+    let element = document.getElementById(e.target.id);
+
+    if(startId===""){
+      startId = e.target.id;
+      element.style.backgroundColor="black";
+      beforeId = e.target.id;
+      return;
+    }
+    justClickedId=e.target.id;
+
+    if(startId===justClickedId){
+      oninit();
+      return;
+    }
+
+    if(parseInt(beforeId)-parseInt(justClickedId)===-1||parseInt(beforeId)-parseInt(justClickedId)===0){
+      if(element.style.backgroundColor!="black"){
+        element.style.backgroundColor="black";
+        beforeId = e.target.id;
+      }
+      else{
+        element.style.backgroundColor="white";
+        beforeId = String(parseInt(justClickedId)-1);
+        console.log(beforeId);
+      }
+    }
+    
+    
+  };
+  const oninit = () => {
+    for(let i=8; i<22; i++){
+      document.getElementById(i).style.backgroundColor="white";
+    }
+    justClickedId = "";
+    startId = "";
+    disable();
+  };
+
+  const disable = () => {
+    for(let i=0; i< data.length; i++){
+
+      let length = parseInt(data[i].endTime)-parseInt(data[i].startTime)
+      for(let j =0; j<length; j++){
+        let index = parseInt(data[i].startTime) + j;
+        document.getElementById(index).disabled = true;
+        console.log(index);
+      }
+
+    }
+    
+  }
+
+ 
 
   return (
     <>
@@ -36,8 +126,9 @@ const ReservationTime = ({ value }) => {
         <ReservationTimeWrapper>
           <Hoursofuse>이용시간</Hoursofuse>
           <Availabletime>최대 5시간 이용가능</Availabletime>
+          <button onClick={oninit}>init</button>
           <SelecttimeWrapper>
-            {times.map((time, i) => (i < 8 || i > 21) ? (<SelecttimeBtn type="danger" disabled>{time}</SelecttimeBtn>) : (<SelecttimeBtn type="danger">{time}</SelecttimeBtn>))}
+            {times.map((time, i) => (i < 8 || i > 21) ? (<SelecttimeBtn type="danger" disabled>{time}</SelecttimeBtn>) : (<SelecttimeBtn type="danger" onClick={onClick}  id={i}>{time}</SelecttimeBtn>))}
           </SelecttimeWrapper>
         </ReservationTimeWrapper>
         : ""}
